fb6484effada761f54893b28bead92b44aa73139
Belalfaouri <belalfaourig@gmail.com>
2018-07-30 11:31:45 +0300

    solved

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -1,21 +1,21 @@
-//Write a stack. Once you’re done, 
-//implement a queue using two stacks to create FIFO behavior. 
+//Write a stack. Once you’re done,
+//implement a queue using two stacks to create FIFO behavior.
 //The queue should not have any storage separate from its stacks.
 
 
 var Stack = function() {
 	var storage = [];
 
-	this.push = function(){
-
+	this.push = function(val){
+		return storage.push(val)
 	};
 
 	this.pop = function(){
-
+		return storage.pop()
 	};
 
 	this.size = function(){
-
+		return storage.length
 	};
 };
 
@@ -24,15 +24,22 @@ var Queue = function() {
 	var inbox = new Stack();
 	var outbox = new Stack();
 
-	this.enqueue = function(){
-
+	this.enqueue = function(val){
+		return inbox.push(val)
 	};
 
 	this.dequeue = function(){
-		
+		if(outbox.size() === 0 ){
+			while(inbox.size !== 0){
+				outbox.push(inbox.pop())
+			}
+		}
+		return outbox.pop()
 	};
 
 	this.size = function(){
+		var totalSize= inbox.size() + outbox.size()
 
+		return totalSize;
 	};
 };
