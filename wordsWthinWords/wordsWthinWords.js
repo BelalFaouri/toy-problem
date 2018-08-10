db1ce108afda743b7d63919f837a5667a131315e
belalfaouri <belalfaourig@gmail.com>
2018-08-10 14:28:43 +0300

    solved

--- wordsWthinWords/wordsWthinWords.js
+++ wordsWthinWords/wordsWthinWords.js
@@ -1,8 +1,36 @@
 //Words within Words
-//Given an array of unique words, find the word that contains the greatest number of other words. 
+//Given an array of unique wordList, find the word that contains the greatest number of other words.
 //A word must be at least two letters long.
 //input:
 //wordList:
-//[ "gray", "grays", "ray", "rays", "strays" ]	
+//[ "gray", "grays", "ray", "rays", "strays" ]
 //output:"grays"
 
+function wordsWithinWords(wordList){
+  var obj={};
+  var max=0;
+  var topWord='';
+
+  for (var i = 0; i < wordList.length; i++) {
+    if(!obj[wordList[i]]){
+      obj[wordList[i]]=0
+    }
+  }
+
+
+    for(var key in obj){
+      for (var i = 0; i < wordList.length; i++) {
+        if(key.indexOf(wordList[i]) !== -1){
+          obj[key]++
+        }
+      }
+  }
+
+  for(var key in obj){
+    if(obj[key] > max){
+      max = obj[key]
+      topWord=key
+    }
+  }
+  return topWord
+}
