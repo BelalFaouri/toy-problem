c861fe9ef5a61fd7a83704e9378f974bd145571b
belalfaouri <belalfaourig@gmail.com>
2018-08-10 16:15:05 +0300

    balanced brackets

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -1,19 +1,55 @@
-//Given a string, return true if its arrangement of parenthesis (), 
+//Given a string, return true if its arrangement of parenthesis (),
 //curly-brackets {}, and square-brackets [] would be considered valid in code and mathematics (is balanced).
 //Examples:
 //Input:"(x + y) - (4)"
 //output:true
 //input:"(((10 ) ()) ((?)(:)))"
 //output:true
-//input:"[{()}]"	
+//input:"[{()}]"
 //output:true
 //input:"(50)("
 //output:false
-//input:"[{]}"	
+//input:"[{]}"
 //output:false
 
 function isBalanced (str) {
-  // Write your code here, and
-  // return your final answer.
-}
+  var stack=[];
 
+  for (var i = 0; i < str.length; i++) {
+    if(str[i] === '(' || str[i] === '[' || str[i] === '{'){
+      stack.push(str[i])
+    }
+    if(str[i] === ')'){
+      var latestItem=stack.pop()
+
+      if(latestItem === '[' || latestItem === '{'){
+        return false
+      }else{
+        stack.pop()
+      }
+    }
+    if(str[i] === ']'){
+      var latestItem=stack.pop()
+
+      if(latestItem === '(' || latestItem === '{'){
+        return false
+      }else{
+        stack.pop()
+      }
+    }
+    if(str[i] === '}'){
+      var latestItem=stack.pop()
+
+      if(latestItem === '[' || latestItem === '('){
+        return false
+      }else{
+        stack.pop()
+      }
+    }
+  }
+  if(stack.length !== 0){
+    return false
+  }
+
+  return true;
+}
