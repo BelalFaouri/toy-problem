72b25768db640a8e23304cd2ff403ff873328119
Belal Faouri <youremail@domain.com>
2018-09-23 18:38:08 +0300

    solved

--- onlyUnique/onlyUnique.js
+++ onlyUnique/onlyUnique.js
@@ -6,18 +6,35 @@ Input                 |	Output
 ------------------------------------
 str:                  |
 "abccdefe"            | "abdf"
-------------------------------------                      
-str:                  | 
+------------------------------------
+str:                  |
 "hello there"         | "o tr"
-------------------------------------                      
+------------------------------------
 str:                  |
 "xyz"	              | "xyz"
-------------------------------------                      
+------------------------------------
 str:                  |
 "iiii"                |	""
 ------------------------------------
 */
 
 function onlyUnique (str) {
-   // your code is here
-}	
+  var letters={};
+  var uniqueLetters='';
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+
+    if(!letters[char]){
+      letters[char] = 1;
+    }else{
+      letters[char]++
+    }
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if(letters[str[i]] === 1){
+      uniqueLetters+=str[i]
+    }
+  }
+  return uniqueLetters
+}
